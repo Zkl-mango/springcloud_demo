95a1fdddb21fbb8b5a5a817eb880057b77702199
Zkl-mango <1473055523@qq.com>
2019-08-16 18:05:18 +0800

    添加新模型

---
+++ springcloud-gateway-helloword/src/test/java/com/zkl/springcloudgatewayhelloword/SpringcloudGatewayHellowordApplicationTests.java
@@ -0,0 +1,16 @@
+package com.zkl.springcloudgatewayhelloword;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringcloudGatewayHellowordApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
